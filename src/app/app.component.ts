435db9091c9f5ed0a1ddd5b74da4e03726052bbf
Swarna <swarnakishoree@gmail.com>
2017-06-28 21:32:25 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit  } from '@angular/core';
+import * as $ from 'jquery';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  title = 'Look jQuery Animation working in action!';
+
+  public ngOnInit()
+  {
+$(document).ready(function(){
+    $("button").click(function(){
+        var div = $("div");  
+        div.animate({left: '100px'}, "slow");
+        div.animate({fontSize: '5em'}, "slow");
+    });
+});
+  }
 }
